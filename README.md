18175c04e3defbceee3d54d95b20c585b251967a
gaedora2 <98475288+gaedora2@users.noreply.github.com>
2022-01-27 00:41:57 +0900

    Add files via upload

---
+++ README.md
@@ -0,0 +1,2 @@
+# bbongdda.github.io
+포트폴리오 사이트 백업
---
+++ stylish-portfolio.js
@@ -0,0 +1,83 @@
+(function($) {
+  "use strict"; // Start of use strict
+
+  // Closes the sidebar menu
+  $(".menu-toggle").click(function(e) {
+    e.preventDefault();
+    $("#sidebar-wrapper").toggleClass("active");
+    $(".menu-toggle > .fa-bars, .menu-toggle > .fa-times").toggleClass("fa-bars fa-times");
+    $(this).toggleClass("active");
+  });
+
+  // Smooth scrolling using jQuery easing
+  $('a.js-scroll-trigger[href*="#"]:not([href="#"])').click(function() {
+    if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+      if (target.length) {
+        $('html, body').animate({
+          scrollTop: target.offset().top
+        }, 1000, "easeInOutExpo");
+        return false;
+      }
+    }
+  });
+
+  var typingBool = false; 
+    var typingIdx=0; 
+    var typingTxt = $(".typing-txt").text(); // 타이핑될 텍스트를 가져온다 
+    typingTxt=typingTxt.split(""); // 한글자씩 자른다. 
+    if(typingBool==false){ // 타이핑이 진행되지 않았다면 
+       typingBool=true; 
+       
+       var tyInt = setInterval(typing,100); // 반복동작 
+     } 
+     
+     function typing(){ 
+       if(typingIdx<typingTxt.length){ // 타이핑될 텍스트 길이만큼 반복 
+         $(".typing").append(typingTxt[typingIdx]); // 한글자씩 이어준다. 
+         typingIdx++; 
+       } else{ 
+         clearInterval(tyInt); //끝나면 반복종료 
+       } 
+     } 
+
+  // Closes responsive menu when a scroll trigger link is clicked
+  $('#sidebar-wrapper .js-scroll-trigger').click(function() {
+    $("#sidebar-wrapper").removeClass("active");
+    $(".menu-toggle").removeClass("active");
+    $(".menu-toggle > .fa-bars, .menu-toggle > .fa-times").toggleClass("fa-bars fa-times");
+  });
+
+  // Scroll to top button appear
+  $(document).scroll(function() {
+    var scrollDistance = $(this).scrollTop();
+    if (scrollDistance > 100) {
+      $('.scroll-to-top').fadeIn();
+    } else {
+      $('.scroll-to-top').fadeOut();
+    }
+  });
+
+})(jQuery); // End of use strict
+
+// Disable Google Maps scrolling
+// See http://stackoverflow.com/a/25904582/1607849
+// Disable scroll zooming and bind back the click event
+var onMapMouseleaveHandler = function(event) {
+  var that = $(this);
+  that.on('click', onMapClickHandler);
+  that.off('mouseleave', onMapMouseleaveHandler);
+  that.find('iframe').css("pointer-events", "none");
+}
+var onMapClickHandler = function(event) {
+  var that = $(this);
+  // Disable the click handler until the user leaves the map area
+  that.off('click', onMapClickHandler);
+  // Enable scrolling zoom
+  that.find('iframe').css("pointer-events", "auto");
+  // Handle the mouse leave event
+  that.on('mouseleave', onMapMouseleaveHandler);
+}
+// Enable map zooming with mouse scroll when the user clicks the map
+$('.map').on('click', onMapClickHandler);
